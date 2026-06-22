c08b573ef86645ec38ec7d4305976845ffc458ed
Rurroo <215533947+Rurroo@users.noreply.github.com>
2026-06-22 23:01:52 +0200

    Added Upgrades

--- lib/main.dart
+++ lib/main.dart
@@ -38,11 +38,22 @@ class _MyHomePageState extends State<MyHomePage> {
   double cookiesPerClick = 1;
   double cookiesPerClickMultiplier = 1;
 
-  int playTime = 0;
+  double playTime = 0;
 
   int cursorsOwned = 0;
+  int cursorCps = 1;
+  double cursorMult = 1;
+  int cursorUpgradeOwned = 0;
+
   int bakersOwned = 0;
+  int bakerCps = 10;
+  double bakerMult = 1;
+  int bakerUpgradeOwned = 0;
+
   int farmsOwned = 0;
+  int farmCps = 100;
+  double farmMult = 1;
+  int farmUpgradeOwned = 0;
 
   int currentPageIndex = 0;
 
@@ -51,9 +62,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState(){
     super.initState();
-    _idleTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+    _idleTimer = Timer.periodic(Duration(milliseconds: 100), (timer) {
+      cookiesPerSecond = (cursorsOwned*cursorCps*cursorMult)+(bakersOwned*bakerCps*bakerMult)+(farmsOwned*farmCps*farmMult);
       idleCookie();
-      playTime++;
+      playTime += 0.1;
     });
   }
   @override
@@ -64,8 +76,8 @@ class _MyHomePageState extends State<MyHomePage> {
   
   void idleCookie(){
     setState((){
-      cookieCount += (cookiesPerSecond * cookiesPerSecondMultiplier);
-      lifetimeCookieCount += (cookiesPerSecond * cookiesPerSecondMultiplier);
+      cookieCount += (cookiesPerSecond * cookiesPerSecondMultiplier)*0.1;
+      lifetimeCookieCount += (cookiesPerSecond * cookiesPerSecondMultiplier)*0.1;
     });
   }
   void clickCookie() {
@@ -74,15 +86,24 @@ class _MyHomePageState extends State<MyHomePage> {
       lifetimeCookieCount += (cookiesPerClick * cookiesPerClickMultiplier);
     });
   }
-  int buyBuilding(double cps,int cost,int amountOwned){
+  int buyBuilding(int cost,int amountOwned){
     if(cookieCount >= cost+(cost*amountOwned*0.2) && amountOwned < 50){
       setState(() {
-        cookiesPerSecond += cps;
-        cookieCount -= cost+(cost*amountOwned*0.2);
         amountOwned++;
+        cookieCount -= cost+(cost*amountOwned*0.2);
       });
     }
     return amountOwned;
+  }
+  dynamic upgradeBuilding(int cost,int upgradeOwned,double mult){
+    if(cookieCount >= cost && upgradeOwned == 0){
+      setState(() {
+        upgradeOwned = 1;
+        cookieCount -= cost;
+        mult++;
+      });
+    }
+    return (upgradeOwned,mult);
   }
 
   @override
@@ -149,57 +170,51 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               Text("BUILDINGS",style: Theme.of(context).textTheme.headlineLarge),
               Card(
-                child:Expanded(
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child:ListTile(
-                          leading: Icon(Icons.pan_tool_alt),
-                          title: Text("Cursor"),
-                          subtitle: Text("Cps: 1  Owned:$cursorsOwned Cost:${(25+25*cursorsOwned*0.2).round()}"),
-                        )
-                      ),
-                      IconButton(
-                          onPressed:(){cursorsOwned = buyBuilding(1, 25, cursorsOwned);}, 
-                          icon: Icon(Icons.add),)
-                    ],
-                  ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child:ListTile(
+                        leading: Icon(Icons.pan_tool_alt),
+                        title: Text("Cursor"),
+                        subtitle: Text("Cps: ${(cursorCps*cursorMult.round())}  Owned:$cursorsOwned Cost:${(25+25*cursorsOwned*0.2).round()}"),
+                      )
+                    ),
+                    IconButton(
+                        onPressed:(){cursorsOwned = buyBuilding(25, cursorsOwned);}, 
+                        icon: Icon(Icons.add),)
+                  ],
                 ),
               ),
               Card(
-                child:Expanded(
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: ListTile(
-                          leading: Icon(Icons.bakery_dining),
-                          title: Text("Baker"),
-                          subtitle: Text("Cps: 10 Owned:$bakersOwned Cost:${(150+150*bakersOwned*0.2).round()}"),
-                        ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ListTile(
+                        leading: Icon(Icons.bakery_dining),
+                        title: Text("Baker"),
+                        subtitle: Text("Cps: ${(bakerCps*bakerMult).round()} Owned:$bakersOwned Cost:${(150+150*bakersOwned*0.2).round()}"),
                       ),
-                      IconButton(
-                        onPressed:(){bakersOwned = buyBuilding(10, 125, bakersOwned);},
-                        icon: Icon(Icons.add),)
-                    ],
-                  ),
+                    ),
+                    IconButton(
+                      onPressed:(){bakersOwned = buyBuilding(125, bakersOwned);},
+                      icon: Icon(Icons.add),)
+                  ],
                 )
               ),
               Card(
-                child:Expanded(
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child:ListTile(
-                          leading: Icon(Icons.agriculture),
-                          title: Text("Farm"),
-                          subtitle: Text("Cps: 50 Owned:$farmsOwned Cost:${(1000+1000*farmsOwned*0.2).round()}"),
-                        )
-                      ),
-                      IconButton(
-                        onPressed:(){farmsOwned = buyBuilding(50, 1000, farmsOwned);},
-                        icon: Icon(Icons.add),)
-                    ],
-                  ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child:ListTile(
+                        leading: Icon(Icons.agriculture),
+                        title: Text("Farm"),
+                        subtitle: Text("Cps: ${(farmCps*farmMult).round()} Owned:$farmsOwned Cost:${(1000+1000*farmsOwned*0.2).round()}"),
+                      )
+                    ),
+                    IconButton(
+                      onPressed:(){farmsOwned = buyBuilding(1000, farmsOwned);},
+                      icon: Icon(Icons.add),)
+                  ],
                 ),
               ),
             ],
@@ -208,15 +223,69 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       //Upgrades screen
       Padding(
-        padding: const EdgeInsets.only(top:100),
-        child: Text("sup"),
+        padding: const EdgeInsets.only(top:75),
+        child: Center(
+          child: Column(
+            children: [
+              Text("UPGRADES",style: Theme.of(context).textTheme.headlineLarge),
+              Card(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child:ListTile(
+                        leading: Icon(Icons.pan_tool_alt),
+                        title: Text("Cursor Upgrade"),
+                        subtitle: Text("Double Cursor Cps Cost: 1000"),
+                      )
+                    ),
+                    IconButton(
+                        onPressed:(){var result = upgradeBuilding(1000, cursorUpgradeOwned, cursorMult); cursorUpgradeOwned = result.$1; cursorMult = result.$2;}, 
+                        icon: Icon(Icons.add),)
+                  ],
+                ),
+              ),
+              Card(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ListTile(
+                        leading: Icon(Icons.bakery_dining),
+                        title: Text("Baker Upgrade"),
+                        subtitle: Text("Double Baker Cps Cost: 5000"),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed:(){var result = upgradeBuilding(5000, bakerUpgradeOwned, bakerMult); bakerUpgradeOwned = result.$1; bakerMult = result.$2;},
+                      icon: Icon(Icons.add),)
+                  ],
+                )
+              ),
+              Card(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child:ListTile(
+                        leading: Icon(Icons.agriculture),
+                        title: Text("Far Upgrade"),
+                        subtitle: Text("Double Farm Cps Cost: 10000"),
+                      )
+                    ),
+                    IconButton(
+                      onPressed:(){var result = upgradeBuilding(10000, farmUpgradeOwned, farmMult); farmUpgradeOwned = result.$1; farmMult = result.$2;},
+                      icon: Icon(Icons.add),)
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
       ),
       //Stats screen
       Padding(
         padding: const EdgeInsets.only(top:100),
         child: Center(
           child: Text(
-            "heyheyhey\nLifetime cockies:${lifetimeCookieCount.round()}\nplaytime:${playTime}s",
+            "heyheyhey\nLifetime cockies:${lifetimeCookieCount.round()}\nplaytime:${playTime.floor()}s",
             style: Theme.of(context).textTheme.headlineMedium
             ),
           ),
